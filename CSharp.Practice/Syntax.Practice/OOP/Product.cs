b4443c77ca870478189732e42f906002c10af197
tamphu.pn <tamphu.pn>
2023-07-06 03:41:10 +0700

    feat: constructor in class

---
+++ CSharp.Practice/Syntax.Practice/OOP/Product.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntax.Practice.OOP
+{
+    /*https://www.simplilearn.com/tutorials/c-sharp-tutorial/c-sharp-constructor
+    Constructor is a special class method that is called very time when a new instance of class was created, it used to set value for property for data member of class
+    We have five type of constructor: 
+    - Default Constructor
+    - Prameterized Constructor
+    - Copy Constructor
+    - Private Constructor
+    - Static Constructor
+    */
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public Product()
+        {
+            // Default constructor is a constructor which do not have any parametered,
+            // and it will be called when an instance of class created, it set number to zero and string to null
+            Console.WriteLine("Default Constructor");
+            Console.WriteLine($@"Name: {Name} and Name is NULL {Name is null}");
+            Console.WriteLine($@"Price: {Price} and Price is Zero {Price is 0}");
+        }
+
+        public Product(string name)
+        {
+            // Parameterized constructor is a constructor which at least one parameter, and it can set difference value for each instance of class
+            Name = name;
+            Console.WriteLine("Parameterlized Constructor");
+        }
+
+        public Product(Product copy)
+        {
+            // Copy Constructor is a constructor to set value for new instance from the existing one
+            Name = copy.Name;
+            Console.WriteLine("Copy Constructor");
+        }
+    }
+
+    public class ProductOwner
+    {
+        public string Name { get; set; }
+        private ProductOwner() 
+        {
+            // Private Constructor is a constructor that is created with private specifier, Other class can not inherited or create an instance of this class.
+            Console.WriteLine("Private Constructor");
+        }
+    }
+
+    // Private Constructor
+    //public class ProductManager: ProductOwner
+    //{
+
+    //}
+
+    public class Item
+    {
+        static Item()
+        {
+            Console.WriteLine("Static Constructor");
+        }
+    }
+}
---
+++ CSharp.Practice/Syntax.Practice/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Syntax.Practice.OOP;
+
+Console.WriteLine("Hello, World!");
+
+// Private Constructor
+// ProductOwner p = new ProductOwner();
+Item a = new Item();
+Item b = new Item();
