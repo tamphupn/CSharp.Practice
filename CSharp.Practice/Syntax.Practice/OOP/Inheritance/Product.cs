f44a93ac472e7b91d57784421e420da16b35b888
tamphu.pn <tamphu.pn>
2023-07-09 23:58:05 +0700

    chore: commite code change

---
+++ CSharp.Practice/Syntax.Practice/OOP/Inheritance/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Syntax.Practice.OOP.Inheritance
+{
+    public class Product
+    {
+    }
+}
