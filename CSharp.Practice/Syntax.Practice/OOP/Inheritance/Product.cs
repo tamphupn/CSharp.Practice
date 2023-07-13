f3186958dd76ce38cf470bc89de1e3bb5a8f32d7
tamphu.pn <tamphu.pn>
2023-07-13 13:55:30 +0700

    update config

--- CSharp.Practice/Syntax.Practice/OOP/Inheritance/Product.cs
+++ CSharp.Practice/Syntax.Practice/OOP/Inheritance/Product.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Syntax.Practice.OOP.Inheritance
 {
-    public class Product
+    public static class Product
     {
+        public static int lonelyinteger(List<int> a)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            int res = -1;
+            foreach (var item in a)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict.Add(item, 1);
+                }
+                else
+                {
+                    dict[item]++;
+                }
+            }
+
+            return dict.Where(x => x.Value == 1).Select(x => x.Key).FirstOrDefault();
+        }
     }
 }
--- CSharp.Practice/Syntax.Practice/Program.cs
+++ CSharp.Practice/Syntax.Practice/Program.cs
@@ -7,3 +7,7 @@ Console.WriteLine("Hello, World!");
 // ProductOwner p = new ProductOwner();
 Item a = new Item();
 Item b = new Item();
+
+List<int> items = new List<int> { 1, 2, 3 };
+var res = items.GroupBy(x => x);
+Console.WriteLine(res);
