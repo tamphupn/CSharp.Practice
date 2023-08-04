4ecc55bdd515de7bdef279f79a601b7ccbe48311
phupham <tamphu.pn@gmail.com>
2023-08-05 00:34:53 +0700

    chore: update another method for one interface with multiple implementation and  example result

--- CSharp.Practice/DependencyInjection/CarService.cs
+++ CSharp.Practice/DependencyInjection/CarService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
-using static System.Formats.Asn1.AsnWriter;
 
 namespace DependencyInjection
 {
@@ -12,6 +11,7 @@ namespace DependencyInjection
     public interface ITransientCarService: ICarService { }
     public interface IScopedCarService : ICarService { }
     public interface ISingletonCarService : ICarService { }
+    public interface ITransientTwoCarService : ICarService { }
 
     public class TransientCarService: ITransientCarService
     {
@@ -26,6 +26,22 @@ namespace DependencyInjection
         public void Process()
         {
             Console.WriteLine($@"{nameof(TransientCarService)} - Process = {No}");
+        }
+    }
+
+    public class TransientTwoCarService : ITransientCarService
+    {
+        private Guid No { get; set; }
+
+        public TransientTwoCarService()
+        {
+            No = Guid.NewGuid();
+            Console.WriteLine($@"{nameof(TransientTwoCarService)} was created - Set No = {No}");
+        }
+
+        public void Process()
+        {
+            Console.WriteLine($@"{nameof(TransientTwoCarService)} - Process = {No}");
         }
     }
 
@@ -66,7 +82,8 @@ namespace DependencyInjection
         public static void Run()
         {
             var serviceProvider = new ServiceCollection()
-                .AddTransient<ITransientCarService, TransientCarService>()
+                .AddScoped<ITransientCarService, TransientCarService>()
+                .AddTransient<ITransientCarService, TransientTwoCarService>()
                 .AddScoped<IScopedCarService, ScopedCarService>()
                 .AddSingleton<ISingletonCarService, SingletonCarService>()
                 .BuildServiceProvider();
@@ -109,6 +126,32 @@ namespace DependencyInjection
                 var singletonService = scopeTwo.ServiceProvider.GetService<ISingletonCarService>() ?? throw new ArgumentNullException(nameof(ISingletonCarService));
                 singletonService.Process();
             }
+
+            /*
+             * Example output
+=== Registed service collection ===
+=== REQUEST ONE ===
+TransientTwoCarService was created - Set No = d646b23c-756d-4754-ac93-122eea2b8e7e
+TransientTwoCarService - Process = d646b23c-756d-4754-ac93-122eea2b8e7e
+TransientTwoCarService was created - Set No = 494164b8-4690-4794-8b7d-ff5fed690e1f
+TransientTwoCarService - Process = 494164b8-4690-4794-8b7d-ff5fed690e1f
+ScopedCarService was created - Set No = 80e1cf69-a39f-4543-b24d-1899912eff94
+ScopedCarService - Process = 80e1cf69-a39f-4543-b24d-1899912eff94
+ScopedCarService - Process = 80e1cf69-a39f-4543-b24d-1899912eff94
+SingletonCarService was created - Set No = a3dfea38-9811-4585-92b9-d25d597ba382
+SingletonCarService - Process = a3dfea38-9811-4585-92b9-d25d597ba382
+=== REQUEST TWO ===
+TransientTwoCarService was created - Set No = 58cf4275-dc85-4f23-844c-e4d3ed6566e9
+TransientTwoCarService - Process = 58cf4275-dc85-4f23-844c-e4d3ed6566e9
+TransientTwoCarService was created - Set No = d0544e55-f186-4f5a-a973-f4b10519a68b
+TransientTwoCarService - Process = d0544e55-f186-4f5a-a973-f4b10519a68b
+ScopedCarService was created - Set No = 7ad1e3e2-d874-4e10-bb75-c87b971b9c1e
+ScopedCarService - Process = 7ad1e3e2-d874-4e10-bb75-c87b971b9c1e
+ScopedCarService - Process = 7ad1e3e2-d874-4e10-bb75-c87b971b9c1e
+SingletonCarService - Process = a3dfea38-9811-4585-92b9-d25d597ba382
+
+             */
+
         }
     }
 }
